0297f3f3af8daaccbcf2ce9357bf569e2cbcdb4f
Harsh Vars <h.harshvats@gmail.com>
2025-12-21 20:37:11 +0530

    Configure the API Bridge

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+// Express Backend URL
+const API_URL = 'http://localhost:5000/api';
+
+const api = axios.create({
+    baseUrl: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    }
+});
+
+// Interceptor: Automatically add the Token to every request
+api.interceptors.request.use((config) => {
+    const token = Cookies.get('token'); // Get token from browser cookies
+    if(token){
+        config.headers['x-auth-token'] = token; // Attach to header
+    }
+    return config;
+}, (error) =>{
+    return Promise.reject(error);
+});
+
+export default api;
